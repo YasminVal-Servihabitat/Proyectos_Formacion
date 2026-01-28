56af3b8b0bda1f6a1b3a025d7f4e2d5171751a64
YasminVal-Servihabitat <yvalverde@servihabitat.com>
2026-01-28 08:52:07 +0100

    Commit 6

--- app/api/get-token.ts
+++ app/api/get-token.ts
@@ -1,20 +1,23 @@
 import { getToken } from "next-auth/jwt"
 import { NextRequest, NextResponse } from "next/server"
 
-export default async function handler(request: NextRequest, res: NextResponse) {
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const token = await getToken({ req: request })
+  
   if (pathname === '/') {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
+  
   if (pathname.startsWith('/dashboard') || pathname.startsWith('/tascas') || pathname.startsWith('/about')) {
-    if (token) {
-      console.log("JSON Web Token", JSON.stringify(token, null, 2))
-      return NextResponse.json({ success: true, token })
-    } else {
-      return NextResponse.json({ error: "NO se ha encontrado ningun TOKEN" }, { status: 401 })
+    if (!token) {
+      return NextResponse.redirect(new URL('/login', request.url));
     }
   }
   
-  return NextResponse.json({ success: true })
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/', '/dashboard/:path*', '/tascas/:path*', '/about/:path*']
 }
